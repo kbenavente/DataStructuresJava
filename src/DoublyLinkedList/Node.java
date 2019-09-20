f8d5fee52617f76c8d1cb3df246a0f6a86c75ad6
kbenavente <benavente.kevin@gmail.com>
2019-09-19 17:43:43 -0700

    Add Node.java for Doubly Linked List

---
+++ src/DoublyLinkedList/Node.java
@@ -0,0 +1,44 @@
+package DoublyLinkedList;
+
+public class Node<E extends Comparable<E>> {
+
+	private E data;
+	private Node<E> left;
+	private Node<E> right;
+
+	public Node() {
+
+		this.data = null;
+		this.left = null;
+		this.right = null;
+
+	}
+
+	public Node(E data) {
+
+		this.data = data;
+		this.left = null;
+		this.right = null;
+
+	}
+
+	/** Returns the {@code Node<E>}'s data.
+	 *
+	 * @return the {@code Node<E>}'s data.
+	 */
+	public E getData() {
+
+		return this.data;
+
+	}
+
+	/** Sets the {@code Node<E>}'s data.
+	 *
+	 */
+	public void setData(E data) {
+
+		this.data = data;
+
+	}
+
+}
